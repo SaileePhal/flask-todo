ceb527dfee24f25e3164cc924c518cea25e776df
SaileePhal <saileemphal@gmail.com>
2025-11-01 02:13:39 +0530

    Created database, and specified columns in app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app =Flask(__name__) #referencing this file
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db' #database file  #3 slashes is for relative path, 4 for absolute path
+db=SQLAlchemy(app) #initializing the database
+
+class Todo(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    content=db.Column(db.String(200),nullable=False)
+    completed=db.Column(db.Integer,default=0)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
 
 @app.route('/')
 def index():
